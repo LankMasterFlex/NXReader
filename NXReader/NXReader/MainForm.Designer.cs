edbab3f5d704cfbc0d15806cf37d855754a9775d
Rajan Grewal <yellondeil2@gmail.com>
2013-09-07 16:52:39 -0400

    first

---
+++ NXReader/NXReader/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace NXReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.pictureBoxCanvas = new System.Windows.Forms.PictureBox();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView.Location = new System.Drawing.Point(0, 24);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(223, 351);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(693, 24);
+            this.menuStrip.TabIndex = 1;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.removeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "NX files|*.nx";
+            this.openFileDialog.Title = "Select a NX file";
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 375);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(693, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(122, 17);
+            this.toolStripStatusLabel.Text = "Selection Type : None";
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(229, 27);
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.ReadOnly = true;
+            this.textBoxInfo.Size = new System.Drawing.Size(452, 20);
+            this.textBoxInfo.TabIndex = 3;
+            // 
+            // pictureBoxCanvas
+            // 
+            this.pictureBoxCanvas.Location = new System.Drawing.Point(229, 53);
+            this.pictureBoxCanvas.Name = "pictureBoxCanvas";
+            this.pictureBoxCanvas.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxCanvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxCanvas.TabIndex = 4;
+            this.pictureBoxCanvas.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 397);
+            this.Controls.Add(this.pictureBoxCanvas);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.statusStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "NXReader";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.PictureBox pictureBoxCanvas;
+    }
+}
+
---
+++ NXReader/NXReader/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using reNX;
+using reNX.NXProperties;
+
+namespace NXReader
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    NXFile nx = new NXFile(openFileDialog.FileName);
+
+                    TreeNode baseTreeNode = new TreeNode(openFileDialog.SafeFileName);
+                    baseTreeNode.Tag = nx;
+                    treeView.Nodes.Add(baseTreeNode);
+
+                    IterateNodes(baseTreeNode, nx.BaseNode);
+                }
+                catch (Exception ex)
+                {
+                    string msg = string.Format("Unable to load {0}{1}{1}{2}", openFileDialog.SafeFileName, Environment.NewLine, ex.ToString());
+                    MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var treeNode = treeView.SelectedNode;
+
+            if (treeNode != null)
+            {
+                var nxFile = treeNode.Tag as NXFile;
+
+                if (nxFile != null)
+                {
+                    treeView.Nodes.Remove(treeNode);
+                    nxFile.Dispose();
+                }
+            }
+        }
+
+        private void IterateNodes(TreeNode parentTreeNode, NXNode parentNXNode)
+        {
+            foreach (NXNode nxNode in parentNXNode)
+            {
+                TreeNode treeNode = new TreeNode(nxNode.Name);
+                treeNode.Tag = nxNode;
+
+                parentTreeNode.Nodes.Add(treeNode);
+
+                IterateNodes(treeNode, nxNode);
+            }
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            NXNode node = e.Node.Tag as NXNode;
+            pictureBoxCanvas.Image = null;
+
+            if (node != null)
+            {
+                string info = string.Empty;
+
+                if (node is NXValuedNode<Point>)
+                {
+                    info = node.ValueOrDie<Point>().ToString();
+                }
+                else if (node is NXValuedNode<string>)
+                {
+                    info = node.ValueOrDie<string>();
+                }
+                else if (node is NXValuedNode<double>)
+                {
+                    info = node.ValueOrDie<double>().ToString();
+                }
+                else if (node is NXValuedNode<long>)
+                {
+                    info = node.ValueOrDie<long>().ToString();
+                }
+                else if (node is NXBitmapNode)
+                {
+                    var bitmap = node.ValueOrDie<Bitmap>();
+                    info = string.Concat("Width = ", bitmap.Width, ", Height = ", bitmap.Height);
+                    pictureBoxCanvas.Image = bitmap;
+                }
+
+                textBoxInfo.Text = info;
+            }
+
+            toolStripStatusLabel.Text = string.Concat("Selection Type : ", e.Node.Tag.GetType().Name);
+        }
+    }
+}
